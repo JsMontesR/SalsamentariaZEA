f8d3d0e4d3850140dbb1805d224d43af50e57838
Sebas <jsmontesr@uqvirtual.edu.co>
2020-08-16 18:58:30 -0500

    Proveedores 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,19 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/circular-std/style.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/libs/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/fontawesome/css/fontawesome-all.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/charts/chartist-bundle/chartist.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/charts/morris-bundle/morris.css') }}">
     <link rel="stylesheet"
           href="{{ asset('assets/vendor/fonts/material-design-iconic-font/css/materialdesignicons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/charts/c3charts/c3.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/flag-icon-css/flag-icon.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/libs/css/footable.bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/datatables/css/dataTables.bootstrap4.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/datatables/css/responsive.bootstrap4.min.css') }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
 </head>
 <body>
 <!-- ============================================================== -->
@@ -189,43 +184,26 @@
                 </div>
             </div>
         </div>
-        <!-- ============================================================== -->
-        <!-- end footer -->
-        <!-- ============================================================== -->
     </div>
-    <!-- ============================================================== -->
-    <!-- end wrapper  -->
-    <!-- ============================================================== -->
 </div>
-<!-- ============================================================== -->
-<!-- end main wrapper  -->
-<!-- ============================================================== -->
-<!-- Optional JavaScript -->
 
-<!-- jquery 3.3.1 -->
-<script src="{{ asset('assets/vendor/jquery/jquery-3.3.1.min.js') }}"></script>
-<!-- bootstap bundle js -->
+
+<script src="{{ asset('assets/vendor/jquery/jquery-3.5.1.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables/js/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/datatables/js/responsive.bootstrap4.min.js') }}"></script>
 <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
-<!-- slimscroll js -->
 <script src="{{ asset('assets/vendor/slimscroll/jquery.slimscroll.js') }}"></script>
-<!-- main js -->
 <script src="{{ asset('assets/libs/js/main-js.js') }}"></script>
-<!-- chart chartist js -->
-<script src="{{ asset('assets/vendor/charts/chartist-bundle/chartist.min.js') }}"></script>
-<!-- sparkline js -->
-<script src="{{ asset('assets/vendor/charts/sparkline/jquery.sparkline.js') }}"></script>
-<!-- morris js -->
-<script src="{{ asset('assets/vendor/charts/morris-bundle/raphael.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/charts/morris-bundle/morris.js') }}"></script>
-<!-- chart c3 js -->
-<script src="{{ asset('assets/vendor/charts/c3charts/c3.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/charts/c3charts/d3-5.4.0.min.js') }}"></script>
-<script src="{{ asset('assets/vendor/charts/c3charts/C3chartjs.js') }}"></script>
-<script src="{{ asset('assets/libs/js/dashboard-ecommerce.js') }}"></script>
-<script src="{{ asset('assets/libs/js/footable.min.js') }}"></script>
+<script src="{{ asset('assets/vendor/jquery/') }}"></script>
+<script>
+    $(document).ready(function() {
+        $('#example').DataTable();
+    } );
+</script>
 </body>
 
----
 {{--<body>--}}
 {{--    <div id="app">--}}
 {{--        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">--}}
--- resources/views/proveedores.blade.php
+++ resources/views/proveedores.blade.php
@@ -7,116 +7,120 @@
     @if(session()->has('success'))
         <div class="alert alert-success" role="alert">{{session('success')}}</div>
     @endif
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Detalle del proveedor</h6>
-        </div>
-        <div class="card-body">
-            <form id="form1" name="form1" method="POST">
-                @csrf
-                <div class="form-group row">
-                    <label class="col-md-4 col-form-label text-md-left">Id:</label>
-                    <div class="col-md-8">
-                        <input readonly="readonly" id="id" class="form-control @error('id') is-invalid @enderror"
-                               value="{{old('id')}}" name="id">
-                        @error('id')
-                        <span class="invalid-feedback" role="alert">
+    <div class="container-fluid">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h3 class="m-0 font-weight-bold text-primary text-center">Detalle del proveedor</h3>
+            </div>
+            <div class="card-body">
+                <form id="form1" name="form1" method="POST">
+                    @csrf
+                    <div class="form-group row">
+                        <label class="col-md-4 col-form-label text-md-left">Id:</label>
+                        <div class="col-md-8">
+                            <input readonly="readonly" id="id" class="form-control @error('id') is-invalid @enderror"
+                                   value="{{old('id')}}" name="id">
+                            @error('id')
+                            <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
-                        @enderror
+                            @enderror
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-md-4 col-form-label text-md-left">Nombre:</label>
+                    <div class="form-group row">
+                        <label class="col-md-4 col-form-label text-md-left">Nombre:</label>
 
-                    <div class="col-md-8">
-                        <input id="nombre" class="form-control @error('nombre') is-invalid @enderror"
-                               value="{{old('nombre')}}" name="nombre" required autocomplete="nombreProveedor">
-                        @error('nombre')
-                        <span class="invalid-feedback" role="alert">
+                        <div class="col-md-8">
+                            <input id="nombre" class="form-control @error('nombre') is-invalid @enderror"
+                                   value="{{old('nombre')}}" name="nombre" required autocomplete="nombreProveedor">
+                            @error('nombre')
+                            <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
-                        @enderror
+                            @enderror
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-md-4 col-form-label text-md-left">Teléfono</label>
+                    <div class="form-group row">
+                        <label class="col-md-4 col-form-label text-md-left">Teléfono</label>
 
-                    <div class="col-md-8">
-                        <input id="telefono" type="number" class="form-control @error('telefono') is-invalid @enderror"
-                               value="{{old('telefono')}}" name="telefono" required autocomplete="telefonoProveedor">
-                        @error('telefono')
-                        <span class="invalid-feedback" role="alert">
+                        <div class="col-md-8">
+                            <input id="telefono" type="number"
+                                   class="form-control @error('telefono') is-invalid @enderror"
+                                   value="{{old('telefono')}}" name="telefono" required
+                                   autocomplete="telefonoProveedor">
+                            @error('telefono')
+                            <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
-                        @enderror
+                            @enderror
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label class="col-md-4 col-form-label text-md-left">Dirección:</label>
-                    <div class="col-md-8">
-                        <input id="direccion" class="form-control " name="direccion" value="{{old('direccion')}}"
-                               required autocomplete="direccionProveedor">
+                    <div class="form-group row">
+                        <label class="col-md-4 col-form-label text-md-left">Dirección:</label>
+                        <div class="col-md-8">
+                            <input id="direccion" class="form-control " name="direccion" value="{{old('direccion')}}"
+                                   required autocomplete="direccionProveedor">
+                        </div>
                     </div>
-                </div>
-            </form>
-            <br>
-            <div class="d-flex justify-content-center">
-                <div align="center" class="btn-toolbar" role="toolbar">
+                </form>
+                <br>
+                <div class="d-flex justify-content-center">
                     <br>
-                    <div class="btn-group col-md">
-                        <input id="registrar" type="button" value="Registrar" class="btn btn-primary"
-                               onclick="registrarProveedor()"/>
-                        <input type="button" value="Limpiar" class="btn btn-secondary" onclick="limpiarCampos()"/>
-                    </div>
-                    <br>
-                    <div class="btn-group col-md">
-                        <input type="button" value="Modificar" class="btn btn-warning" onclick="modificarCliente()"/>
-                        <input type="button" value="Eliminar" class="btn btn-danger" onclick="eliminarCliente()"/>
+                    <div class="row btn-toolbar" role="toolbar">
+
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 py-2">
+                            <input id="registrar" type="button" value="Registrar"
+                                   class="btn btn-primary container-fluid"
+                                   onclick="registrarProveedor()"/>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 py-2">
+                            <input type="button" value="Limpiar" class="btn btn-secondary container-fluid"
+                                   onclick="limpiarCampos()"/>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 py-2">
+                            <input type="button" value="Modificar" class="btn btn-warning container-fluid"
+                                   onclick="modificarCliente()"/>
+                        </div>
+                        <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 py-2">
+                            <input type="button" value="Eliminar" class="btn btn-danger container-fluid"
+                                   onclick="eliminarCliente()"/>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Proveedores registrados</h6>
-        </div>
-        <div class="card-body">
-            <div class="table-responsive">
-                @if(!$clientes->isEmpty())
-                    <table class="table table-bordered" width="100%" cellspacing="0" data-page-length='5'>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h3 class="m-0 font-weight-bold text-primary text-center">Proveedores registrados</h3>
+            </div>
+            <div class="card-body">
+                @if(!$proveedores->isEmpty())
+                    <table id="example" class="table table-striped table-bordered dt-responsive nowrap"
+                           style="width:100%" cellspacing="0" data-page-length='5'>
                         <thead>
                         <tr>
-                            <th>Seleccionar</th>
                             @foreach ($proveedores->get(0) as $key => $value)
                                 <th>{{$key}}</th>
                             @endforeach
+                            <th>Seleccionar</th>
                         </tr>
                         </thead>
                         <tbody>
                         @foreach($proveedores as $registro)
                             <tr>
+                                @foreach ($registro as $key => $value)
+                                    <td>{{ $value }}</td>
+                                @endforeach
                                 <td align="center">
                                     <a id="{{$registro->Id}}" class="btn btn-secondary text-white" href="#page-top">
                                         <em class="fas fa-angle-up"></em>
                                         Ver
                                     </a>
                                 </td>
-                                @foreach ($registro as $key => $value)
-                                    <td>{{ $value }}</td>
-                                @endforeach
                             </tr>
                         @endforeach
                         </tbody>
-                        <tfoot>
-                        <tr>
-                            <th>Seleccionar</th>
-                            @foreach ($proveedores->get(0) as $key => $value)
-                                <th>{{$key}}</th>
-                            @endforeach
-                        </tr>
-                        </tfoot>
                     </table>
                 @else
                     <h3 align="center">No hay proveedores disponibles, intentelo más tarde</h3>
@@ -124,5 +128,6 @@
             </div>
         </div>
     </div>
-    <script src="{{asset('public/assets/libs/js/proveedores.js')}}"
+
+    {{--    <script src="{{asset('public/assets/libs/js/proveedores.js')}}"--}}
 @endsection
