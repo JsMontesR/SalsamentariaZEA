6505557a73084635338f39f749b71071487f1baa
Sebas <jsmontesr@uqvirtual.edu.co>
2020-11-12 16:20:38 -0500

    Registros

--- public/assets/libs/js/spanish.js
+++ public/assets/libs/js/spanish.js
@@ -1,5 +1,5 @@
 let options = {
-    "aLengthMenu": [[5, 10, 25, 50, 100 - 1], [5, 10, 25, 50, 100, "Todos"]],
+    "aLengthMenu": [[5, 10, 25, 50, 100, 500, 1000, - 1], [5, 10, 25, 50, 100, 500, 1000, "Todos"]],
     "language": {
         "sProcessing": "Procesando...",
         "sLengthMenu": "Mostrar _MENU_ registros",
